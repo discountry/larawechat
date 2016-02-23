2570e4a0c374159a8e773f195f24f39037d3e28a
unknown <>
2016-02-23 23:31:19 +0800

    weui

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,18 +1,21 @@
 <!-- Stored in resources/views/layouts/master.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
-        <title>App Name - @yield('title')</title>
-        <link rel="stylesheet" href="public/css/weui.min.css"/>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <title>App Name - @yield('title')</title>
+    <link rel="stylesheet" href="public/css/weui.min.css"/>
+    <link rel="stylesheet" href="public/css/example.css"/>
+</head>
     <body>
 
             @yield('content')
 
     </body>
     <script src="public/js/zepto.min.js"></script>
+    <script src="public/js/example.js"></script>
+</body>
 </html>
 
     
