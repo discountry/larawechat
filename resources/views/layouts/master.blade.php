1409e94eff6f4b5d5a7eaddc1ced5d863c1265e8
discountry <331027793@qq.com>
2016-02-25 17:02:37 +0800

    no-publick

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
     <title>App Name - @yield('title')</title>
-    <link rel="stylesheet" href="public/css/weui.min.css"/>
-    <link rel="stylesheet" href="public/css/example.css"/>
+    <link rel="stylesheet" href="css/weui.min.css"/>
+    <link rel="stylesheet" href="css/example.css"/>
 </head>
     <body>
 
             @yield('content')
 
     </body>
-    <script src="public/js/zepto.min.js"></script>
-    <script src="public/js/example.js"></script>
+    <script src="js/zepto.min.js"></script>
+    <script src="js/example.js"></script>
 </body>
 </html>
 
